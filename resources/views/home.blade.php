af764bdffea9601cf049e7585eb892b1d71193ff
Alfredo <adf.palermo@icloud.com>
2021-01-07 21:29:27 +0100

    step 1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.structure')
+
+@section('content')
+<main>
+     <div class="backgroundhero">
+         <div class="container">
+            <img src="{{ asset('img/cover-home.jpg')}}" alt="cover">
+         </div>        
+     </div>
+     <section class="comics">
+         <div class="container">
+             <ul>
+                 @foreach ($comics as $comic)
+                 <li>
+                     <a href=""> <img src="{{$comic['image']}}" alt="{{$comic['title']}}">{{$comic['title']}}</a>
+                 </li>                     
+                 @endforeach
+             </ul>
+         </div>
+     </section>
+</main>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+<header>
+    <div class="linetopheader">
+        <div class="container flexright">
+            <ul>
+                <li>DC POWER VISA</li>
+                <li>ADDICTIONAL DC SITES</li>
+            </ul>
+        </div>
+    </div>
+    <div class="navbarheader container flexbetween">
+        <img src="{{ asset('img/logo.png')}}" alt="logo">
+        <ul class="flexbetween">
+            <li>characters</li>
+            <li>comics</li>
+            <li>movie</li>
+            <li>tv</li>
+            <li>games</li>
+            <li>collectibles</li>
+            <li>video</li>
+            <li>fans</li>
+            <li>new</li>
+            <li>shop</li>
+        </ul>
+        <div>
+            Search
+        </div>
+    </div>
+</header>
